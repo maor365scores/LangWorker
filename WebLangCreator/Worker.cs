e4ffc7f5df69994c666c9a86d9c30a4c02f5c526
Maor Rocky <maor@365scores.com>
2023-01-19 12:14:00 +0200

    saving changes.

--- WebLangCreator/Worker.cs
+++ WebLangCreator/Worker.cs
@@ -5,8 +5,8 @@ namespace WebLangCreator;
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly IServiceProvider _serviceProvider;
     private SportifierDB _context;
-    private IServiceProvider _serviceProvider;
 
     public Worker(ILogger<Worker> logger, IServiceProvider serviceProvider)
     {
@@ -24,8 +24,11 @@ public class Worker : BackgroundService
             {
                 var scopedProcessingService = scope.ServiceProvider.GetRequiredService<SportifierDB>();
                 _context = scopedProcessingService;
-                await CreateAthletesInWebLang();
-                await CreateCompetitorsInWebLang();
+
+                await Task.WhenAll(CreateAthletesInWebLang(),
+                                   CreateCompetitorsInWebLang(),
+                                   CreateCompetitionInWebLang(),
+                                   CreateCountriesInWebLang());
             }
 
             await Task.Delay(10000, stoppingToken);
@@ -41,10 +44,10 @@ public class Worker : BackgroundService
         }
 
         var athletesNameId = await _context.Athletes
-                                     .Where(a => !_context.DictValues.Any(x => x.TermId == a.NameId &&
-                                                                               x.LangId == webLang.LanguageId))
-                                     .Select(x => x.NameId)
-                                     .ToListAsync();
+                                           .Where(a => !_context.DictValues.Any(x => x.TermId == a.NameId &&
+                                                                                     x.LangId == webLang.LanguageId))
+                                           .Select(x => x.NameId)
+                                           .ToListAsync();
 
         foreach (var nameId in athletesNameId)
         {
@@ -60,7 +63,7 @@ public class Worker : BackgroundService
             _logger.LogInformation("Added athlete {Name} to web lang", englishValue);
         }
     }
-    
+
     public async Task CreateCompetitorsInWebLang()
     {
         var webLang = await _context.Languages.FirstOrDefaultAsync(x => x.Iso3LettersCode.Equals("WEB"));
@@ -70,10 +73,68 @@ public class Worker : BackgroundService
         }
 
         var nameIds = await _context.Competitors
-                                           .Where(competitor => !_context.DictValues.Any(x => x.TermId == competitor.NameId &&
-                                                                                     x.LangId == webLang.LanguageId))
-                                           .Select(x => x.NameId)
-                                           .ToListAsync();
+                                    .Where(competitor => !_context.DictValues.Any(x => x.TermId == competitor.NameId &&
+                                                                                       x.LangId == webLang.LanguageId))
+                                    .Select(x => x.NameId)
+                                    .ToListAsync();
+
+        foreach (var nameId in nameIds)
+        {
+            var englishValue = _context.DictValues.FirstOrDefault(x => x.TermId == nameId && x.LangId == 1)?.Value;
+            if (string.IsNullOrWhiteSpace(englishValue))
+            {
+                continue;
+            }
+
+            await
+                _context.Database
+                        .ExecuteSqlInterpolatedAsync($"EXECUTE [Dict].[AddValue] {nameId} , {webLang.LanguageId} , {englishValue} ,{1},{0} ,{null}");
+            _logger.LogInformation("Added athlete {Name} to web lang", englishValue);
+        }
+    }
+
+    public async Task CreateCompetitionInWebLang()
+    {
+        var webLang = await _context.Languages.FirstOrDefaultAsync(x => x.Iso3LettersCode.Equals("WEB"));
+        if (webLang is null)
+        {
+            return;
+        }
+
+        var nameIds = await _context.Competitions
+                                    .Where(competition => !_context.DictValues.Any(x => x.TermId == competition.NameId &&
+                                                                                        x.LangId == webLang.LanguageId))
+                                    .Select(x => x.NameId)
+                                    .ToListAsync();
+
+        foreach (var nameId in nameIds)
+        {
+            var englishValue = _context.DictValues.FirstOrDefault(x => x.TermId == nameId && x.LangId == 1)?.Value;
+            if (string.IsNullOrWhiteSpace(englishValue))
+            {
+                continue;
+            }
+
+            await
+                _context.Database
+                        .ExecuteSqlInterpolatedAsync($"EXECUTE [Dict].[AddValue] {nameId} , {webLang.LanguageId} , {englishValue} ,{1},{0} ,{null}");
+            _logger.LogInformation("Added athlete {Name} to web lang", englishValue);
+        }
+    }
+
+    public async Task CreateCountriesInWebLang()
+    {
+        var webLang = await _context.Languages.FirstOrDefaultAsync(x => x.Iso3LettersCode.Equals("WEB"));
+        if (webLang is null)
+        {
+            return;
+        }
+
+        var nameIds = await _context.Countries
+                                    .Where(country => !_context.DictValues.Any(x => x.TermId == country.NameId &&
+                                                                                    x.LangId == webLang.LanguageId))
+                                    .Select(x => x.NameId)
+                                    .ToListAsync();
 
         foreach (var nameId in nameIds)
         {
